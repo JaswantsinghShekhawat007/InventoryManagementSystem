934be0d9982210a8e62e25189b097d65eadc71b2
Jaswantsingh Shekhawat <56820958+JaswantsinghShekhawat007@users.noreply.github.com>
2024-03-27 16:47:02 +0530

    Delete InventoryyManagement_Frontend/ims directory

--- InventoryyManagement_Frontend/ims/src/services/Auth-Admin.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const AUTH_REST_API_BASE_URL = "http://localhost:8085/api/auth-admin";
-
-
-export const deleteMerchantCall = (merchantId) => axios.delete(AUTH_REST_API_BASE_URL+`/delete-merchant/${merchantId}`);
-
-export const deleteAdminCall = (adminId) => axios.delete(AUTH_REST_API_BASE_URL+`/delete/${adminId}`);
--- InventoryyManagement_Frontend/ims/src/services/MerchantService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { getLoggedInUser } from './AuthService';
-
-const AUTH_REST_API_BASE_URL = "http://localhost:8085/api/merchant";
-
-const merchantId = getLoggedInUser();
-export const updateMerchantCall = (merchantObj) => axios.put(AUTH_REST_API_BASE_URL+`/update-merchant/${merchantId}`,merchantObj);
